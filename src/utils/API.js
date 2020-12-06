32acb8674154ed84079a9cd9a39766c81bde55ed
cboswel1 <christianboswell86@gmail.com>
2020-12-05 22:03:00 -0700

    api comment

--- src/utils/API.js
+++ src/utils/API.js
@@ -2,6 +2,8 @@ import axios from "axios";
 
 // https://randomuser.me/api/?results=200&nat=us
 
+// changed for var export due to eslint error
+
 export const getRandomEmployee = function () {
     return axios.get("https://randomuser.me/api/?results=20&nat=us");
   };
